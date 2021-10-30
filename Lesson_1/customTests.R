b817fe1ac33354a097af60ab974f81b46bdbc8a5
Greg White <greg.white@cbs.curtin.edu.au>
2021-10-30 21:01:25 +0800

    dataframe

--- Lesson_1/customTests.R
+++ Lesson_1/customTests.R
@@ -49,7 +49,8 @@ submit_log <- function(){
                         stringsAsFactors = FALSE)
   write.csv(log_tbl, file = temp, row.names = FALSE)
   write.csv(log_tbl, file = "temp.csv", row.names = FALSE)
-  write_sheet("temp.csv")
+  temp.df <- data.frame("temp.csv")
+  write_sheet(temp.df)
   #  encoded_log <- base64encode(temp)
   # browseURL(paste0(pre_fill_link, encoded_log))
 }
