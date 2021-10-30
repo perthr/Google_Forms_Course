50e41d7b24d0378d62e236eebede79b024c5d8fb
Greg White <greg.white@cbs.curtin.edu.au>
2021-10-30 20:49:33 +0800

    edit ss argument for googlesheet4

--- Lesson_1/customTests.R
+++ Lesson_1/customTests.R
@@ -49,7 +49,7 @@ submit_log <- function(){
                         stringsAsFactors = FALSE)
   write.csv(log_tbl, file = temp, row.names = FALSE)
   write.csv(log_tbl, file = "temp.csv", row.names = FALSE)
-  write_sheet()
+  write_sheet(ss = pre_fill_link)
   #  encoded_log <- base64encode(temp)
   # browseURL(paste0(pre_fill_link, encoded_log))
 }
