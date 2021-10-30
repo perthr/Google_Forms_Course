71bbfa25df41e4ee2b6543b708979bec5d8a067a
Greg White <greg.white@cbs.curtin.edu.au>
2021-10-30 23:20:42 +0800

    Update customTests.R

--- Lesson_1/customTests.R
+++ Lesson_1/customTests.R
@@ -50,8 +50,7 @@ submit_log <- function(){
   # write.csv(log_tbl, file = temp, row.names = FALSE)
   # write.csv(log_tbl, file = "temp.csv", row.names = FALSE)
   # temp.df <- data.frame(temp)
-  ss <- gs4_create(name = "Besty_Bob2", sheets = c(1:20))
-  sheet_write(log_tbl, ss)
+  sheet_write(log_tbl,ss = pre_fill_link,sheet = Sheet2)
   # encoded_log <- base64encode(temp)
   # browseURL(paste0(pre_fill_link, encoded_log))
 }
