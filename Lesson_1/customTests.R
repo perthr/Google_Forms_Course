291939cbf6da7bb7fd144d41e32d7a2f2de4dea1
Greg White <greg.white@cbs.curtin.edu.au>
2021-10-30 21:20:44 +0800

    Update customTests.R

--- Lesson_1/customTests.R
+++ Lesson_1/customTests.R
@@ -49,7 +49,6 @@ submit_log <- function(){
                         stringsAsFactors = FALSE)
   write.csv(log_tbl, file = temp, row.names = FALSE)
   write.csv(log_tbl, file = "temp.csv", row.names = FALSE)
-  temp.df <- data.frame("temp.csv")
   read_sheet(ss = temp)
   #  encoded_log <- base64encode(temp)
   # browseURL(paste0(pre_fill_link, encoded_log))
